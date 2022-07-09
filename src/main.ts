adc36f2d4b9c1f42353d6f5b6f9b4666c26159e7
choral7451 <chorales@naver.com>
2022-07-09 13:15:33 +0900

    Update https cookie set test5

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModuleLocal } from './app.moduleLocal';
 import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModuleLocal);
+  const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
